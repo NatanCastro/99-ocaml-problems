adb1b8e9e7daea207f8d61bee2a51c9b0c066f24
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2023-12-04 18:37:47 -0300

    feat: add text decription

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,15 @@
 let () =
   let arr = [ 1; 2; 3; 4; 5 ] in
+  print_string "last func: ";
   Ex1.last arr |> Option.get |> print_int;
   print_newline ();
+  print_string "last_two func: ";
   Ex2.last_two arr |> Option.get |> fun t ->
   print_int (fst t);
+  print_string ", ";
   print_int (snd t);
   print_newline ();
+  print_string "at func: ";
   Ex3.at arr 2 |> Option.get |> print_int;
   print_newline ();
   ()
